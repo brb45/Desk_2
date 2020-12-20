2a75dc684694d15fe83879bba8c62fdd164b8188
brb45 <sunusd@yahoo.com>
2020-12-20 00:50:53 -0800

    12-20-20 2

--- log_test.py
+++ log_test.py
@@ -1,26 +1,4 @@
-def maxNonOverlapping(self, nums: List[int], target: int) -> int:
 
-    n = len(nums)
-    sums = [0]*n
-    res = [0] * n
-    max_len = 0
-    dic = {}
-
-    for i in range(n):
-        sums[i] = sums[i-1] + nums[i] if i > 0 else nums[i]
-
-    for i, v in enumerate(nums):
-        val = v - target
-        if val in dic:
-            res[i] = res[dic[val]] + 1
-        elif val == 0:
-            res[i] = 1
-
-        dic[v] = i
-        if i > 0:
-            res[i] = max(res[i], res[i-1])
-
-    return max(res)
 
 
 
