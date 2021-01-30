ca1593ec0bddb9f2d2c858d65dc6034c84248a8d
brb45 <sunusd@yahoo.com>
2021-01-30 13:02:02 -0800

    1/30/21

--- log_test.py
+++ log_test.py
@@ -1,16 +1,23 @@
-def nextLargerNodes(self, head, G) -> List[int]:
-
-    cnt = 0
-    flag = 0
-    while head and head.next:
-        if head.val in G and head.next.val in G:
-            if flag == 0:
-                cnt += 2
-                flag = 1
-            else:
-                cnt += 1
-        else:
-            flag = 0
-        head = head.next
-    return cnt
+class Solution:
+    def insertionSortList(self, head: ListNode) -> ListNode:
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        rev_head = None
+        while slow:
+            node_next = slow.next
+            slow.next, rev_head = rev_head, slow.next
+            slow = node_next
+        cur = dummy = ListNode()
+        while rev_head:
+            cur.next = head
+            head = head.next
+            cur.next.next = rev_head
+            cur = rev_head
+            rev_head = rev_head.next
+        if head:
+            cur.next = head
+            head.next = None
+        return dummy.next
 
